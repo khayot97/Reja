d5ad0c22ef8c3f780abf21d275dc4631281e5de1
Khayotbek <khayotbekabdurashidov97gmail.com>
2024-11-17 17:16:33 +0900

    feat: trains callback hamd  async functions

--- trains.js
+++ trains.js
@@ -8,8 +8,7 @@ const list = [
     "endi dam oling, foydasi yoq endi", // 60 
 ];
 
-// console.log("xato:============", list);
-
+// CALLBACK function
 function maslahatBering(a, callback) {
     if(typeof a !== "number") callback("insert a number", null);
     else if(a <= 20) callback(null, list[0]);
@@ -19,17 +18,69 @@ function maslahatBering(a, callback) {
     else if(a > 50 && a <= 60) callback(null, list[4]);
     else if(a > 20 && a <= 30) callback(null, list[5]);
     else {
-        setTimeout(function () {
+        setInterval(function () {
             callback(null, list[5]);
-        }, 5000);
+        }, 1000);
     }   
 }
 
 console.log("paasd here 0");
-maslahatBering(65, (err, data) => {
-    if(err) console.log('ERROR:', err);
+maslahatBering(70, (err, data) => {
+    if (err) console.log("ERROR:", err);
     else {
-        console.log('javob:', data);
+        console.log(data);
     }
 });
-console.log("paasd here 1");
+console.log("passed here 1");
+
+// ASYNC function
+// async function maslahatBering(a) {
+//     if(typeof a !== "number") throw new Error("insert a number", null);
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         setInterval(function () {
+//             callback(null, list[5]);
+//         }, 1000);
+//     }
+// }
+//         return new Promise((resolve, reject) => {
+//             setInterval(() => {
+                
+//             }, interval);(() => {
+//                 resolve(list[5]);
+//             }, 1000);
+//         });
+//         // setTimeout(function () {
+//         //     return(null, list[5]);
+//         // }, 5000);
+//     }   
+// }
+
+// CALL VIA then va catch ishadi!
+// console.log("paasd here 0");
+// maslahatBering(65)
+//     .then((data) => {
+//         console.log("javob:", data);
+// })
+//     .catch((err) => {
+//     console.log("ERROR:", err);
+// });
+// console.log("passed here 1");
+
+
+// CALL VIA async va await ishladi qachonki javobni olganda keyin ishledi yani kete ketlida ishledi!
+// async function run () {
+//     let javob = await maslahatBering(25);
+//     console.log(javob);
+// }
+
+//     javob = await maslahatBering(70);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
