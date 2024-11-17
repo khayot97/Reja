cbdf8a4e4e1ddf751244b7e8a6fbc741c4c04262
Khayotbek <khayotbekabdurashidov97gmail.com>
2024-11-17 16:14:06 +0900

    feat: NodeJS event loop va Callback functionlar

--- trains.js
+++ trains.js
@@ -1,3 +1,35 @@
+console.log("Jack Ma maslahatlari");
+const list = [
+    "yaxshi talab boling", // 0-20 yoshgacha
+    "togri boshliq tanglang va koproq hato qiling", // 20 - 30 tanlov
+    "uziningizga ishlashingizni boshlang", // 30 - 40 harakat 
+    "siz kuchli bolgan narsalarni  qiling", // 40 - 50 ishlash 
+    "yoshlarga investitsiya qiling", // 50 - 60 farzandlarga
+    "endi dam oling, foydasi yoq endi", // 60 
+];
 
+// console.log("xato:============", list);
 
-console.log('train task ishga tushdi!');
+function maslahatBering(a, callback) {
+    if(typeof a !== "number") callback("insert a number", null);
+    else if(a <= 20) callback(null, list[0]);
+    else if(a > 20 && a <= 30) callback(null, list[1]);
+    else if(a > 30 && a <= 40) callback(null, list[2]);
+    else if(a > 40 && a <= 50) callback(null, list[3]);
+    else if(a > 50 && a <= 60) callback(null, list[4]);
+    else if(a > 20 && a <= 30) callback(null, list[5]);
+    else {
+        setTimeout(function () {
+            callback(null, list[5]);
+        }, 5000);
+    }   
+}
+
+console.log("paasd here 0");
+maslahatBering(65, (err, data) => {
+    if(err) console.log('ERROR:', err);
+    else {
+        console.log('javob:', data);
+    }
+});
+console.log("paasd here 1");
